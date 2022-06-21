bc7791cfc5bf183ebff0ce11735f5450d718f81a
Git-foramit <94371014+Git-foramit@users.noreply.github.com>
2022-06-21 10:48:00 +0200

    Pushing new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="quicksample123",                     # This is the name of the package
-    version="0.0.2",                        # The initial release version
+    version="0.0.3",                        # The initial release version
     author="Amit Joshi",                     # Full name of the author
     description="Quicksample Test Package",
     long_description=long_description,      # Long description read from the the readme file
